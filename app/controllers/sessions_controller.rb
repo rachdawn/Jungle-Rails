bb1f85c348ce47429e91b87b94adf0a724ee33b7
Rachel Little <racheldawnlittle@gmail.com>
2023-12-21 14:59:03 -0700

    update to authenticate_with_credentials and show alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    
+    if user
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      redirect_to '/login', alert: 'Invalid email or password'
     end
   end
 
