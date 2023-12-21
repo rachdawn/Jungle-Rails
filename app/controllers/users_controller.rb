2318c10bbf2a9f3f044454a9dd3419b903b46e6f
Rachel Little <racheldawnlittle@gmail.com>
2023-12-21 14:55:14 -0700

    update user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 end
